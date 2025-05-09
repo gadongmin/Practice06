d8b79e2b360c5ed0c1d89c4a47c62341a5b937aa
gadongmin <dmkim777@naver.com>
2025-05-09 17:40:20 +0900

    20250509(1)

--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -1,7 +1,30 @@
 package com.javaex.ex05;
 
-public class Depart {
+public class Depart extends Employee {
+	// 필드
+	private String depart;
 
-	 //코드작성
+	// 생성자
+	public Depart() {
+	}
+
+	public Depart(String name, int salary, String depart) {
+		super(name, salary);
+		this.depart = depart;
+	}
+
+	// 메소드-gs
+	public String getDepart() {
+		return depart;
+	}
+
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+
+	// 메소드-일반
+	public void showInformation() {
+		System.out.println("이름: " + super.getName() + " 연봉: " + super.getSalary() + " 부서: " + this.depart);
+	}
 
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -1,17 +1,21 @@
 package com.javaex.ex07;
 
 public class SmartPhone extends MusicPhone {
-    
-    public void execute(String str){
-        
-        //코드작성
-        
-    }
- 
-    //메소드작성
-    
-    //메소드작성
-    
-    
-    
+
+	public void execute(String str) {
+
+		if ("음악".equals(str)) {
+			playMusic();
+		} else if ("앱".equals(str)) {
+			app();
+		} else {
+			super.execute(str);
+		}
+
+	}
+
+	// 메소드작성
+	public void app() {
+		System.out.println("다운로드해서 음악재생");
+	}
 }
